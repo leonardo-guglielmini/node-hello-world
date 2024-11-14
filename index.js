d9bb1b5ed8e97e2fa78e581293ffedfd829fcff4
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-11-14 13:57:45 +0100

    ADD console log args

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+//console.log("Hello World");
+
+//console.log(process.argv);
+
+let words = process.argv.slice(2).map((arg) => {
+    return arg;
+});
+
+//console.log(words);
+console.log(`Hello ${words.join(" ")}`);
